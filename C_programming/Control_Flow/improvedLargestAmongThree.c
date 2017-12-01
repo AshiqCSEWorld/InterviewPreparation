03a3cd25a5e8326d1374867bdc101b120bd3a7a8
Ashiqur Rahman <ashiqur15-5470@diu.edu.bd>
2017-12-02 03:26:32 +0600

    Improvised version of finding largest among three program

---
+++ C_programming/Control_Flow/improvedLargestAmongThree.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+  int n1,n2,n3;
+  while((scanf("%d%d%d",&n1,&n2,&n3) == 3)) {
+        if(n1 > n2) {
+         
+           if(n1>n3) {
+               printf("%d is the largest number\n",n1);
+          } else {
+               printf("%d is the largest number\n",n3);
+          }     
+         
+      }
+
+      else {
+         if(n2 > n3) {
+            printf("%d is the largest number\n",n2);
+          
+         } else {
+             printf("%d is the largest number\n",n3);
+          
+       }
+ 
+
+     } 
+   
+  }
+  
+  return 0;
+}
