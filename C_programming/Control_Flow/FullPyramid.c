282742eab83db7c1e4ff75dcceb875142951cecc
Ashiqur Rahman <ashiqur15-5470@diu.edu.bd>
2017-12-09 13:02:15 +0600

    Program to print full pyramid

---
+++ C_programming/Control_Flow/FullPyramid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+ int i,j,k,numOfRows;
+ printf("Enter number of rows: ");
+ scanf("%d",&numOfRows);
+
+ for(i=1; i<=numOfRows; i++) {
+   for(j=i; j<numOfRows; j++) {
+     printf(" ");
+   }
+
+   for(k=1; k<(2*i); k++) {
+     printf("*");
+   }
+   printf("\n");
+ }
+
+ return 0;
+
+}
